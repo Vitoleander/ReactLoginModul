cad7a0ff90b5a8c77ec0e099f4af5509be8c0653
Vitoleander <89918725+Vitoleander@users.noreply.github.com>
2022-10-05 13:18:10 +0200

    Finish the last code

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -3,6 +3,7 @@ import { useNavigate } from "react-router-dom";
 
 const Dashboard = () => {
     const navigate = useNavigate();
+    const token = localStorage.getItem('username');
 
     const handleLogout = () => {
         localStorage.removeItem('username');
@@ -11,17 +12,21 @@ const Dashboard = () => {
         navigate('/');
     }
 
+    if(token) {
     return (
         <div className="app">
             <div className="Title-conatiner">
                 <h1 className="Title">Dashboard</h1>
-                <h2>Welcome! You are logged in!</h2>
+                <h2 className="Login">Welcome! You are logged in!</h2>
             </div>
             <div className="Link-container">
                 <button onClick={handleLogout}>Log out</button>
             </div>
         </div>
     )
+    } else {
+        alert('Page not avalibe without signing in')
+    }
 }
 
 export default Dashboard;
